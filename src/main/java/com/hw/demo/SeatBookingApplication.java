45520408fa8a2f9c7a4ebf115d784bc04b75b9b6
Sunilchaurha@gmail.com <Sunilchaurha@gmail.com>
2017-07-08 10:42:49 +0530

    initial commit

---
+++ src/main/java/com/hw/demo/SeatBookingApplication.java
@@ -0,0 +1,12 @@
+package com.hw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatBookingApplication.class, args);
+	}
+}
